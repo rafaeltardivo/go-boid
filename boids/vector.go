05fac6b6e4e01ba79a8272731c5a4a01de95dacc
Rafael Tardivo <rafaeldt.br@gmail.com>
2022-06-25 21:20:10 -0300

    Add Add function to Vector struct

--- boids/vector.go
+++ boids/vector.go
@@ -1,6 +1,15 @@
 package boids
 
-type vector struct {
-	x float64
-	y float64
+// Vector (two dimentional definition).
+type Vector struct {
+	X float64
+	Y float64
+}
+
+// Add adds v (X, Y) to operand (X, Y).
+func (v Vector) Add(operand Vector) Vector {
+	return Vector{
+		X: v.X + operand.X,
+		Y: v.Y + operand.Y,
+	}
 }
